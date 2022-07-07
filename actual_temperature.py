68460688823abd99c5466bfbece941ea8adeb9fb
satohitoshi1 <satohitoshi121@gmail.com>
2022-07-07 16:04:26 +0900

    もうちょい

--- actual_temperature.py
+++ actual_temperature.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 import os
 import datetime
+from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 import csv
 import urllib.request
 from bs4 import BeautifulSoup
 
-# CSVいらない疑惑
 
-today = datetime.date.today()
-yesterday = today + relativedelta(days=-1)
+# CSVいらない疑惑i
+# データ取得開始・終了日
+yesterday = datetime.datetime.today() - relativedelta(days=1)  # 気象庁の更新が昨日までなので
+end_month = yesterday.month
+# 日付条件の設定
+strdt = datetime.datetime.strptime("2022-01-01", "%Y-%m-%d")  # 開始日
+enddt = datetime.date.strftime(yesterday, "%Y-%m-%d")  # 終了日 いらない予感
+
+# 日付差の日数を算出（リストに最終日も含めたいので＋１）
+days_num = (yesterday - strdt).days + 1
+# シンプルにforとappendを使用した場合
+
+
+days_list = []
+for w in range(days_num):
+    x = strdt + timedelta(days=w)
+    y = datetime.date.strftime(x, "%Y-%m-%d")
+    days_list.append(y)
 
 
 def str2float(weather_data):
@@ -20,7 +36,6 @@ def str2float(weather_data):
 
 
 def scraping(url, date):
-
     # 気象データのページを取得
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html)
@@ -30,15 +45,6 @@ def scraping(url, date):
     data_list_per_hour = []
 
     # table の中身を取得
-
-    # ボツ1
-    #    tags = soup.find_all('tr', class_='mtx')
-    #    tbs = [tag.text for tag in tags]
-
-    # ボツ2
-    #    for tr in trs.find_all("tr")[2:]:
-    #        tds = tr.find_all("td")
-
     trs = soup.find_all("tr", class_="mtx")[3:]
 
     for tr in trs:
@@ -46,8 +52,7 @@ def scraping(url, date):
         if tds[1].string is None:
             break
 
-        data_list.append(date)
-        data_list.append(tds[0].string)
+        data_list.append(days_list)
         data_list.append(str2float(tds[1].string))
         data_list.append(str2float(tds[2].string))
         data_list.append(str2float(tds[3].string))
@@ -80,13 +85,9 @@ def create_csv():
     # 出力ファイル名
     output_file = "weather.csv"
 
-    # データ取得開始・終了日
-    start_date = datetime.date(2022, 1, 1)  # あとで出穂の日からに直す
-    end_date = yesterday  # 気象庁の更新が昨日までなので
-
     # CSV の列
     fields = [
-        "日",
+        "年月日",
         "降水量(合計/mm)",
         "降水量(最大1時間/mm)",
         "降水量(最大10分間/mm)",
@@ -102,28 +103,21 @@ def create_csv():
         "風向",
         "最多風向",
         "日照時間(h)",
-        "降雪の深さの合計(m)" "最深積雪(m)",
+        "降雪の深さの合計(m)",
+        "最深積雪(m)",
     ]
 
     with open(os.path.join(output_dir, output_file), "w") as f:
         writer = csv.writer(f, lineterminator="\n")
         writer.writerow(fields)
-        date = start_date
-
-        while date != end_date + datetime.timedelta(1):  #
-
-            # 鶴岡市
-            url = (
-                "https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=35&block_no=0263&year=2022&month=0%s&day=1&view="
-                % (date.month)
-            )
-
-            data_per_day = scraping(url, date)
-
+        for mon in range(1, end_month + 1):  # URLのmonth==〇〇にいれる変数とりあえずrangeで
+            # year=2022の部分も改良の余地ありあとで
+            search_url = f"https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=35&block_no=0263&year=2022&month={mon}&day=1&view="
+            data_per_day = scraping(search_url, strdt)
             for dpd in data_per_day:
                 writer.writerow(dpd)
-
-            relativedelta(months=1)
+            if mon == end_month:
+                break
 
 
 if __name__ == "__main__":
